0bc1cd2c7606858b01f70bccda44f345925bdf0a
Alvin Ng <alvinng@Alvin-Ngs-MacBook-Pro.local>
2023-03-02 18:33:19 +0800

    FINALLY

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,40 @@
-public class Song {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private ArrayList<String> listenersRecord;
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+        this.listenersRecord = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> listeners) {
+        int total = 0;
+
+        for (int i = 0; i < listeners.size(); i++) {
+            String currListener = listeners.get(i).toLowerCase();
+
+            if(!listenersRecord.contains(currListener)) {
+                listenersRecord.add(currListener);
+                total++;
+            }
+        }
+
+        return total;
+    }
 }
